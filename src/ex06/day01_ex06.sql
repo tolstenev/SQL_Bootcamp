8f42819e899747ea5280ab94d171c09f5158b213
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-07 20:47:10 +0000

    ex06 is done

---
+++ src/ex06/day01_ex06.sql
@@ -0,0 +1,11 @@
+SELECT order_date AS action_date,
+	(SELECT name FROM person
+     WHERE id = person_order.person_id) AS person_name
+FROM person_order
+UNION
+SELECT visit_date AS action_date,
+ 	(SELECT name FROM person
+ 	 WHERE id = person_visits.person_id) AS person_name
+FROM person_visits
+ORDER BY action_date, person_name DESC
+;

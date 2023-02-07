c002fd78229e2392d3c837a43f4662e03e99585e
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-07 14:42:32 +0300

    ex06 is done

---
+++ src/ex06/day00_ex06.sql
@@ -0,0 +1,14 @@
+SELECT name,
+       CASE
+           WHEN name = 'Denis' THEN 'true'
+           ELSE 'false'
+           END AS check_name
+FROM (SELECT (SELECT name
+              FROM person
+              WHERE id = person_order.person_id) AS NAME
+      FROM person_order
+      WHERE (menu_id = 13 OR
+             menu_id = 14 OR
+             menu_id = 18)
+        AND (order_date = '2022-01-07')) AS CHECK1
+;

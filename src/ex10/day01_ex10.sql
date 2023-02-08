f722600331aba104ebd444fce9dc592ca98feea3
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-08 15:11:05 +0300

    ex10 is done

---
+++ src/ex10/day01_ex10.sql
@@ -0,0 +1,10 @@
+SELECT (SELECT name FROM person
+        WHERE id = person_order.person_id)    AS person_name,
+       (SELECT pizza_name FROM menu
+        WHERE menu.id = person_order.menu_id) AS pizza_name,
+       (SELECT name FROM pizzeria
+        WHERE id = menu.pizzeria_id)          AS pizzeria_name
+FROM person_order
+JOIN menu ON menu.id = person_order.menu_id
+ORDER BY person_name, pizza_name, pizzeria_name
+;

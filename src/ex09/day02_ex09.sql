3c7f7354b662663f6fe9a3a49f23789ea4c1a1c4
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-09 19:13:04 +0300

    ex09 style fix

--- src/ex09/day02_ex09.sql
+++ src/ex09/day02_ex09.sql
@@ -1,17 +1,17 @@
 SELECT DISTINCT person.name
-        FROM menu
-                 JOIN pizzeria p ON menu.pizzeria_id = p.id
-                 JOIN person_order po ON menu.id = po.menu_id
-                 JOIN person ON person.id = po.person_id
-                 JOIN person_visits pv ON person.id = pv.person_id
-        WHERE gender = 'female'
-          AND pizza_name = 'cheese pizza'
+FROM menu
+         JOIN pizzeria p ON menu.pizzeria_id = p.id
+         JOIN person_order po ON menu.id = po.menu_id
+         JOIN person ON person.id = po.person_id
+         JOIN person_visits pv ON person.id = pv.person_id
+WHERE gender = 'female'
+  AND pizza_name = 'cheese pizza'
 INTERSECT
 SELECT DISTINCT person.name
- FROM menu
-          JOIN pizzeria p ON menu.pizzeria_id = p.id
-          JOIN person_order po ON menu.id = po.menu_id
-          JOIN person ON person.id = po.person_id
-          JOIN person_visits pv ON person.id = pv.person_id
- WHERE gender = 'female'
-   AND pizza_name = 'pepperoni pizza'
+FROM menu
+         JOIN pizzeria p ON menu.pizzeria_id = p.id
+         JOIN person_order po ON menu.id = po.menu_id
+         JOIN person ON person.id = po.person_id
+         JOIN person_visits pv ON person.id = pv.person_id
+WHERE gender = 'female'
+  AND pizza_name = 'pepperoni pizza'

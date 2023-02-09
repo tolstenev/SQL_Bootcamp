91124e7e52b4ba203d4f7b69d41d4c190c0fe475
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-09 16:54:00 +0300

    ex03 is done

---
+++ src/ex03/day02_ex03.sql
@@ -0,0 +1,15 @@
+WITH cte_day_gen AS (
+    SELECT missing_date::date
+    FROM GENERATE_SERIES('2022-01-01'::date, '2022-01-10'::date, '1 day') missing_date
+)
+
+SELECT missing_date
+FROM cte_day_gen
+         LEFT JOIN (SELECT visit_date
+                    FROM person_visits
+                    WHERE person_id = 1
+                       OR person_id = 2) AS visits_person_id_1_and_2
+                   ON visits_person_id_1_and_2.visit_date = missing_date
+WHERE visit_date ISNULL
+ORDER BY missing_date
+;

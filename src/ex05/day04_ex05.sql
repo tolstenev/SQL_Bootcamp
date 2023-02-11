9d7a8d124f30a23adc1a632295b4bc0be4f667af
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-11 18:32:34 +0300

    ex05 is done

---
+++ src/ex05/day04_ex05.sql
@@ -0,0 +1,6 @@
+SELECT name, pizza_name, price, round(price - price*0.1) AS discount_price
+from person_order po
+    JOIN menu m ON m.id = po.menu_id
+    JOIN person p ON p.id = po.person_id
+ORDER BY name, pizza_name
+;

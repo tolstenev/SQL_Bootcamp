787e305db3289e9a6ff1b75c0a742dccbae1113c
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-15 21:04:01 +0300

    ex06 is done

---
+++ src/ex06/day06_ex06.sql
@@ -0,0 +1,7 @@
+CREATE SEQUENCE seq_person_discounts START 1 INCREMENT 1;
+
+SELECT SETVAL('seq_person_discounts', MAX(id))
+FROM person_discounts;
+
+ALTER TABLE person_discounts
+    ALTER COLUMN id SET DEFAULT NEXTVAL('seq_person_discounts');

3d6451470e73d563fad5b35dfa685349745ea7ce
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-15 21:04:45 +0300

    Some changes

--- src/ex01/day06_ex01.sql
+++ src/ex01/day06_ex01.sql
@@ -1,13 +1,13 @@
-INSERT INTO person_discounts (id, person_id, pizzeria_id, discount)
-SELECT ROW_NUMBER() OVER ()                    ,
-       person_id                               ,
-       m.pizzeria_id                           ,
+-- INSERT INTO person_discounts (id, person_id, pizzeria_id, discount)
+SELECT ROW_NUMBER() OVER ()                    AS id,
+       po.person_id                            AS person_id,
+       m.pizzeria_id                           AS person_id,
        CASE
            WHEN COUNT(person_id) = 1 THEN 10.5
            WHEN COUNT(person_id) = 2 THEN 22
            ELSE 30
-           END
-FROM person_order
-         JOIN menu m ON person_order.menu_id = m.id
+           END                                 AS discount
+FROM person_order po
+         JOIN menu m ON po.menu_id = m.id
 GROUP BY person_id, pizzeria_id
-ORDER BY person_id
+ORDER BY po.person_id, m.pizzeria_id
--- src/ex02/day06_ex02.sql
+++ src/ex02/day06_ex02.sql
@@ -1,11 +1,11 @@
 SELECT p.name,
        pizza_name,
        price,
-       ROUND(price - price * pd.discount * 0.01)::integer AS discount_price,
-       p2.name                                            AS pizzeria_name
+       price - price * pd.discount * 0.01 AS discount_price,
+       p2.name                            AS pizzeria_name
 FROM person_order po
          JOIN menu m ON m.id = po.menu_id
          JOIN person p ON p.id = po.person_id
          JOIN pizzeria p2 ON p2.id = m.pizzeria_id
-         JOIN person_discounts pd ON p.id = pd.person_id
-ORDER BY name, pizza_name;
+         JOIN person_discounts pd ON (p.id = pd.person_id AND p2.id = pd.pizzeria_id)
+ORDER BY p.name, pizza_name;

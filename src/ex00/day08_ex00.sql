9ea53762e88af11bf10c63a5535797cb91ca9e0d
Yonn Argelia <yonnarge@student.21-school.ru>
2023-02-17 14:06:55 +0300

    ex00 was changed

--- src/ex00/day08_ex00.sql
+++ src/ex00/day08_ex00.sql
@@ -1,36 +1,37 @@
 -- Session #1
--- INPUT
 SELECT * FROM pizzeria WHERE name = 'Pizza Hut';
--- OUTPUT
 --  id |   name    | rating
 -- ----+-----------+--------
 --   1 | Pizza Hut |    4.6
 
 -- Session #2
--- INPUT
 SELECT * FROM pizzeria WHERE name = 'Pizza Hut';
--- OUTPUT
 --  id |   name    | rating
 -- ----+-----------+--------
 --   1 | Pizza Hut |    4.6
 
 -- Session #1
--- INPUT
 START TRANSACTION;
-UPDATE pizzeria SET rating = 5 WHERE name = 'Pizza Hut';
-SELECT * FROM pizzeria WHERE name = 'Pizza Hut';
-
--- OUTPUT
 -- START TRANSACTION
+UPDATE pizzeria SET rating = 5 WHERE name = 'Pizza Hut';
 -- UPDATE 1
+SELECT * FROM pizzeria WHERE name = 'Pizza Hut';
 --  id |   name    | rating
 -- ----+-----------+--------
 --   1 | Pizza Hut |      5
 
 -- Session #2
--- INPUT
 SELECT * FROM pizzeria WHERE name = 'Pizza Hut';
--- OUTPUT
 --  id |   name    | rating
 -- ----+-----------+--------
 --   1 | Pizza Hut |    4.6
+
+-- Session #1
+COMMIT;
+-- COMMIT
+
+-- Session #2
+SELECT * FROM pizzeria WHERE name = 'Pizza Hut';
+--  id |   name    | rating
+-- ----+-----------+--------
+--   1 | Pizza Hut |      5
